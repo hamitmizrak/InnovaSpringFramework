6aa50050eecf815de5f38dc7316b57a7019a344f
hamitmizrak <hamitmizrak@gmail.com>
2022-01-16 18:34:14 +0300

    Stereotype = çok tane Anotaiton tek bir bizim oluşturduğumuz anaotation oluştururuz.

---
+++ src/main/java/com/hamitmizrak/b2/stereotype/StudentBean.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak.b2.stereotype;
+
+@InnovaMultipleAnontaiton
+// Stereotype çok Anotation teke indirir.
+public class StudentBean {
+	
+	private String name = "Deneme";
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
